ca8db659ee9fd5f7e3e9ed8a72d3993e93e73d24
土屋将一 <tsuchiyashouichi@tsuchiyashouichinoMacBook-Pro.local>
2021-05-05 22:18:20 +0900

    make projects dir and tail wind css settings

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+// import '../styles/globals.css'
+import 'tailwindcss 2/tailwind.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
